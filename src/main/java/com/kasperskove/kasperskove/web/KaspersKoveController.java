693cc9c81b1506de376f218f5c6fc65aafcfad06
kasper95 <kove_kasper@outlook.com>
2017-06-03 09:38:24 -0400

    +controller, bootstrap, index.html

---
+++ src/main/java/com/kasperskove/kasperskove/web/KaspersKoveController.java
@@ -0,0 +1,17 @@
+package com.kasperskove.kasperskove.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class KaspersKoveController {
+
+    @RequestMapping (value = "/", method = RequestMethod.GET)
+    public String index () {
+
+        return "index";
+    }
+
+
+}

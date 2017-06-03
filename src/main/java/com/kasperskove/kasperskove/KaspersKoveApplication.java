50055bbce9b4a73f8dd3cfae954796a1494c6d63
kasper95 <kove_kasper@outlook.com>
2017-06-03 07:39:55 -0400

    initial commit

---
+++ src/main/java/com/kasperskove/kasperskove/KaspersKoveApplication.java
@@ -0,0 +1,12 @@
+package com.kasperskove.kasperskove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaspersKoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaspersKoveApplication.class, args);
+	}
+}
